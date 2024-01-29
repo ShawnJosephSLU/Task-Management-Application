8cbfe4244c032e17ed643b9063865cc5491a7e0c
Shawn Joseph <87461949+ShawnJosephSLU@users.noreply.github.com>
2024-01-29 18:03:16 -0400

    Update Header.tsx

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -8,19 +8,16 @@ const Header = () => {
         <AppBar>
             <Toolbar style={{ justifyContent: 'space-between' }}>
 
-                {/* TODO LIST Header on the Left */}
                 <Typography variant="h5" fontFamily="fantasy" noWrap>
                     TODO LIST
                 </Typography>
 
-                {/* Centered Navigation Links */}
                 <div style={{ display: 'flex', justifyContent: 'center', flexGrow: 1 }}>
                     <NavLink to="/dashboard"><Button style={{ color: '#ffffff' }}>Home</Button></NavLink>
                     <NavLink to='/mytasks'><Button style={{ color: '#ffffff' }}>My Created Tasks</Button></NavLink>
                     <NavLink to='/create-task'><Button style={{ color: '#ffffff' }}>Create New Task</Button></NavLink>
                 </div>
 
-                {/* "Sign Out" Button on the Right */}
                 <NavLink to='/signin'><Button style={{ color: '#ffffff' }}>Sign Out</Button></NavLink>
 
             </Toolbar>
