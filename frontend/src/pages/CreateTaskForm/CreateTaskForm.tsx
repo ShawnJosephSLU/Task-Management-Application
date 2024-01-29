7d876f442cc4ac0be840adcae4ff91d559b19f20
Shawn Joseph <87461949+ShawnJosephSLU@users.noreply.github.com>
2024-01-29 19:25:29 -0400

    update New Task Form

--- frontend/src/pages/CreateTaskForm/CreateTaskForm.tsx
+++ frontend/src/pages/CreateTaskForm/CreateTaskForm.tsx
@@ -1,31 +1,40 @@
 // TaskForm
+import { Autocomplete, Button, FormGroup,TextField, Typography } from "@mui/material";
 import Header from "../../components/Header/Header";
+import { useEffect, useState } from 'react';
+
+
 
 const CreateTaskForm = () => {
     // stores the tasks 
- 
+    // TODO:  The date should not be a date before the current date
+    const userList = [{displayName : "Shawn"}, {displayName : "Amber"}, {displayName : "April"}];
 
     return (
         <>
-            <Header/>
+            <Header />
             <h1>Create New Task</h1>
-            <form>
-                    <input placeholder="Title" type="text" />
-                    <input placeholder="Description" type="" />
-                    <input placeholder="Assignee" type="text" />
-                    <div>
-                        <p>Notes</p>
-                        <input placeholder="Enter a Note" type="text" />
-                        
-                    </div>
-                    <div>
-                        <p>Due Date</p>
-                        <input  type="date" />
-                    </div>
-                    
-                    <button type="submit">Cancel</button>
-                    <button type="submit">Create</button>
-            </form>
+            <FormGroup>
+                <TextField placeholder="Title" type="text" />
+                <TextField placeholder="Description" type="" />
+                <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={userList}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label="Assignee" />}
+                    />
+                <TextField placeholder="Enter a Note" type="text" />
+                
+                <Typography>Due Date</Typography>
+                <TextField type="date" />
+
+                <div>
+                    <Button type="submit">Cancel</Button>
+                    <Button type="submit">Create</Button>
+                </div>
+
+            </FormGroup>
         </>
     );
 }
